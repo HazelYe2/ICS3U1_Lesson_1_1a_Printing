e7417a8f431d11d3d9041680179f1492234b2ad9
HazelYe2 <Hazel.Ye25@ycdsbk12.ca>
2024-02-08 11:47:20 -0500

    UpdatedPrintingComments.java

--- PrintingComments.java
+++ PrintingComments.java
@@ -8,14 +8,25 @@ class PrintingComments extends ConsoleProgram {
   public void run() {
 
     // Say hello
-    
+    System.out.println("Hi");
+    System.out.println("There");
 
     // Say hello on the same line
+    System.out.print("Hi ");
+    System.out.print("There ");
     
     
     // multiline comment
-    
-    
+    /* This is 
+    * a multi-line 
+    * comment */
+
+    System.out.println("*************");
+    System.out.println("*           *");
+    System.out.println("*           *");
+    System.out.println("*           *");
+    System.out.println("*************");
+      
 
   }
 }
